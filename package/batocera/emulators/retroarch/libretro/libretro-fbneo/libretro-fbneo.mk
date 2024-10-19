1125ca9f5a1a0e2eac294ca7eed0e3b39e29c23a
aleksei74 <aleksei74@gmail.com>
2024-10-19 20:21:59 +0900

    DsNo님 FBNeo 업데이트 (1019)

--- package/batocera/emulators/retroarch/libretro/libretro-fbneo/libretro-fbneo.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-fbneo/libretro-fbneo.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Feb 24, 2024
-LIBRETRO_FBNEO_VERSION = 4a555e062b3a388dfc0893515cde78ce0b0b3909
+LIBRETRO_FBNEO_VERSION = 14e69be6d1472d631407375411ca7493e223f066
 LIBRETRO_FBNEO_SITE = $(call github,libretro,FBNeo,$(LIBRETRO_FBNEO_VERSION))
 LIBRETRO_FBNEO_LICENSE = Non-commercial
 
